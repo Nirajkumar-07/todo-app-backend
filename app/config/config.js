1de6ac48e702cffb89f9611ef9fdc082b8d01315
Nirajkumar-07 <vidyut3711@gmail.com>
2025-08-15 14:11:52 +0530

    `Update config.js to require SSL for PostgreSQL connection and remove unused migration files.`

--- app/config/config.js
+++ app/config/config.js
@@ -5,10 +5,10 @@ module.exports = {
     url: process.env.DATABASE_URL,
     dialect: "postgres",
     dialectOptions: {
-      //   ssl: {
-      //     require: true,
-      //     rejectUnauthorized: false,
-      //   },
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
   },
   production: {
